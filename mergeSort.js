e31e8a1e38f7c52461b803c022140bad43a3e2e2
Dylan Simon <grvdylans@gmail.com>
2025-03-06 23:20:23 -0700

    Finished project

--- mergeSort.js
+++ mergeSort.js
@@ -1,13 +1,37 @@
 function mergeSort(arr) {
-    // base case
-    if (arr.length = 1)
-        return;
+  // base case
+  if (arr.length == 1) return arr;
 
+  //recursive case
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let sortedArray = [];
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] > right[rightIndex]) {
+        sortedArray.push(right[rightIndex]);
+      rightIndex++;
+    } else {
+        sortedArray.push(left[leftIndex]);
+      leftIndex++;
+    }
+  }
 
+  
+  while (leftIndex < left.length) {
+    sortedArray.push(left[leftIndex]);
+    leftIndex++;
+  }
+  while (rightIndex < right.length) {
+    sortedArray.push(right[rightIndex]);
+    rightIndex++;
+  }
+  return sortedArray;
 }
 
-// if there's one, return
-// else split it, and run mergeSort on the children
-// run slice(0, (length - (length % 2 == 1 ? 1 : 0))/2)
-// and run slice((length - (length % 2 == 1 ? 1 : 0))/2 + 1)
-// take each return, and merge them
+let badArray = [9,12,4,13,100,200,99,1]
+console.log(badArray)
+let goodArray = mergeSort(badArray)
+console.log(goodArray)
